a77961bb04f0cfc9f35805ea52c1d5a6352bb84b
Ahmed Abdullah <137221373+ahmedembeddedx@users.noreply.github.com>
2024-02-07 21:43:14 +0500

    Update Image_Segmentation.py

--- CV_Scripts/Image_Segmentation.py
+++ CV_Scripts/Image_Segmentation.py
@@ -10,3 +10,23 @@ img = cv.erode(img, (5, 5), iterations=1)
 
 cv.imshow("X-RAY", img)
 cv.waitKey(0)
+
+
+import cv2 as cv
+
+live_vid = cv.VideoCapture(0)
+
+while True:
+    isTure, frame = live_vid.read()
+    
+    frame = cv.GaussianBlur(frame, (3, 3), cv.BORDER_DEFAULT)
+    frame = cv.Canny(frame, 100, 200)
+    frame = cv.dilate(frame, (7, 7), iterations=3) 
+    frame = cv.erode(frame, (3, 3), iterations=3)
+    
+    cv.imshow("Video", frame)
+    if cv.waitKey(20) & 0xFF==ord('0'):
+        break
+    
+live_vid.release()
+cv.destroyAllWindows()
